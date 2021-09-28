e8c5a09e957a829c35bc89b9239715a423f7a51b
thomkrillis <thisisjackolantern@gmail.com>
2021-09-28 19:40:05 -0400

    Fix tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
+import { store } from '../../app/store';
 
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders title text', () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Home />
+      </Router>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/find your weather/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/shared/SearchBar/SearchBar.test.tsx
+++ src/shared/SearchBar/SearchBar.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import { store } from '../../app/store';
 
 test('renders input', () => {
-  render(<SearchBar />);
+  render(
+    <Provider store={store}>
+      <Router>
+        <SearchBar />
+      </Router>
+    </Provider>
+  );
   const inputElement = screen.getByRole('textbox');
   expect(inputElement).toBeInTheDocument();
 });
